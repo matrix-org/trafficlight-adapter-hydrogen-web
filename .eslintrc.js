97f7401160419eced4c013a73e68ce4e8ce36912
RMidhunSuresh <hi@midhun.dev>
2022-10-26 14:51:00 +0530

    Iniital Commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2020,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": "off",
+        "no-empty": "off",
+        "no-prototype-builtins": "off",
+        "no-unused-vars": "warn"
+    },
+};
